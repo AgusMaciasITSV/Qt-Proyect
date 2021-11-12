9384ab7c98651184d66bc52b0e453ab140915b93
nachofillol05 <nachofillol05@gmail.com>
2021-11-11 21:51:21 -0300

    UwU

---
+++ formpro.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'formybWpJO.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+from datetime import date, datetime
+
+
+class Ui_form(object):
+
+    def setupUi(self, form):
+        if not form.objectName():
+            form.setObjectName(u"form")
+        form.resize(544, 188)
+        self.centralwidget = QWidget(form)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayoutWidget = QWidget(self.centralwidget)
+        self.gridLayoutWidget.setObjectName(u"gridLayoutWidget")
+        self.gridLayoutWidget.setGeometry(QRect(0, 0, 541, 186))
+        self.gridLayout = QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.nombreline = QLineEdit(self.gridLayoutWidget)
+        self.nombreline.setObjectName(u"nombreline")
+
+        self.gridLayout.addWidget(self.nombreline, 0, 0, 1, 1)
+
+        self.apellidoline = QLineEdit(self.gridLayoutWidget)
+        self.apellidoline.setObjectName(u"apellidoline")
+
+        self.gridLayout.addWidget(self.apellidoline, 0, 1, 1, 1)
+
+        self.dniline = QLineEdit(self.gridLayoutWidget)
+        self.dniline.setObjectName(u"dniline")
+
+        self.gridLayout.addWidget(self.dniline, 1, 0, 1, 1)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.botonborrar = QPushButton(self.gridLayoutWidget)
+        self.botonborrar.setObjectName(u"botonborrar")
+
+        self.horizontalLayout.addWidget(self.botonborrar)
+
+        self.botonagregar = QPushButton(self.gridLayoutWidget)
+        self.botonagregar.setObjectName(u"botonagregar")
+
+        self.horizontalLayout.addWidget(self.botonagregar)
+
+        self.gridLayout.addLayout(self.horizontalLayout, 4, 1, 1, 1)
+
+        self.fechaseleccion = QDateEdit(self.gridLayoutWidget)
+        self.fechaseleccion.setObjectName(u"fechaseleccion")
+        self.fechaseleccion.setMinimumDateTime(QDateTime(QDate(1921, 9, 14), QTime(0, 0, 0)))
+        self.fechaseleccion.setMaximumDateTime(QDateTime(QDate(2021, 11, 17), QTime(0, 0, 0)))
+
+        self.gridLayout.addWidget(self.fechaseleccion, 2, 0, 1, 1)
+
+        self.ciudadseleccion = QComboBox(self.gridLayoutWidget)
+        self.ciudadseleccion.addItem("")
+        self.ciudadseleccion.addItem("")
+        self.ciudadseleccion.addItem("")
+        self.ciudadseleccion.addItem("")
+        self.ciudadseleccion.setObjectName(u"ciudadseleccion")
+        self.ciudadseleccion.setEditable(False)
+        self.ciudadseleccion.setSizeAdjustPolicy(QComboBox.AdjustToContents)
+
+        self.gridLayout.addWidget(self.ciudadseleccion, 4, 0, 1, 1)
+
+        self.vacunaseleccion = QComboBox(self.gridLayoutWidget)
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.addItem("")
+        self.vacunaseleccion.setObjectName(u"vacunaseleccion")
+
+        self.gridLayout.addWidget(self.vacunaseleccion, 1, 1, 1, 1)
+
+        form.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(form)
+        self.botonborrar.clicked.connect(form.borrar)
+        self.botonagregar.clicked.connect(form.agregar)
+
+        QMetaObject.connectSlotsByName(form)
+
+    # setupUi
+
+    def retranslateUi(self, form):
+        form.setWindowTitle(QCoreApplication.translate("form", u"MainWindow", None))
+        self.nombreline.setText("")
+        self.nombreline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el nombre", None))
+        self.apellidoline.setInputMask("")
+        self.apellidoline.setText("")
+        self.apellidoline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el apellido", None))
+        self.dniline.setText("")
+        self.dniline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el DNI", None))
+        self.botonborrar.setText(QCoreApplication.translate("form", u"Borrar", None))
+        self.botonagregar.setText(QCoreApplication.translate("form", u"agregar", None))
+        self.ciudadseleccion.setItemText(0, QCoreApplication.translate("form", u"seleccione ciudad", None))
+        self.ciudadseleccion.setItemText(1, QCoreApplication.translate("form", u"La calera", None))
+        self.ciudadseleccion.setItemText(2, QCoreApplication.translate("form", u"Villa allende", None))
+        self.ciudadseleccion.setItemText(3, QCoreApplication.translate("form", u"Narnia", None))
+
+        self.ciudadseleccion.setCurrentText(QCoreApplication.translate("form", u"seleccione ciudad", None))
+        self.vacunaseleccion.setItemText(0, QCoreApplication.translate("form", u"Seleccione la vacuna", None))
+        self.vacunaseleccion.setItemText(1, QCoreApplication.translate("form", u"SPUTNIK V(1era dosis)", None))
+        self.vacunaseleccion.setItemText(2, QCoreApplication.translate("form", u"SPUTNIK V(2da dosis)", None))
+        self.vacunaseleccion.setItemText(3, QCoreApplication.translate("form", u"ASTRAZENECA(1era dosis)", None))
+        self.vacunaseleccion.setItemText(4, QCoreApplication.translate("form", u"ASTRAZENECA(2da dosis)", None))
+        self.vacunaseleccion.setItemText(5, QCoreApplication.translate("form", u"PFIZER(1era dosis)", None))
+        self.vacunaseleccion.setItemText(6, QCoreApplication.translate("form", u"PFIZER(2da dosis)", None))
+        self.vacunaseleccion.setItemText(7, QCoreApplication.translate("form", u"MODERNA(1era dosis)", None))
+        self.vacunaseleccion.setItemText(8, QCoreApplication.translate("form", u"MODERNA(2da dosis)", None))
+        self.vacunaseleccion.setItemText(9, QCoreApplication.translate("form", u"SINOPHARM(1era dosis) ", None))
+        self.vacunaseleccion.setItemText(10, QCoreApplication.translate("form", u"SINOPHARM(2da dosis) ", None))
+
+    # retranslateUi
--- main.py
+++ main.py
@@ -1,20 +1,70 @@
 import sys
-from PySide2.QtWidgets import QMainWindow, QApplication
 
-from su_archivo_convertido_desde_ui import nombre_de_la_clase_en_el_archivo_convertido_de_ui
+from PyQt5.QtCore import QCoreApplication
+from PySide2.QtWidgets import QMainWindow, QApplication, QMessageBox
+
+from formpro import Ui_form
 
 
-class MainWindow(QMainWindow):
+class Formwindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = nombre_de_la_clase_en_el_archivo_convertido_de_ui()
+        self.ui = Ui_form()
         self.ui.setupUi(self)
+
+    def agregar(self):
+        data = {
+            'nombre': self.ui.nombreline.text(),
+            'apellido': self.ui.apellidoline.text(),
+            'documento': self.ui.dniline.text(),
+            'fecha': self.ui.fechaseleccion.date(),
+            'ciudad': self.ui.ciudadseleccion.currentText(),
+            'vacuna': self.ui.vacunaseleccion.currentText(),
+        }
+        count_before = len(self.parent.students)
+        self.parent.LATABLA.append(data)
+        print(self.parent.LATABLA)
+        if count_before != len(self.parent.LATABLA):
+            message = QMessageBox()
+            message.setText(f"Se agrego el vacunado {data['nombre']} exitosamente")
+            message.exec()
+
+    def borrar(self):
+        self.ui.nombreline.setText("")
+        self.ui.nombreline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el nombre", None))
+        self.ui.apellidoline.setInputMask("")
+        self.ui.apellidoline.setText("")
+        self.ui.apellidoline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el apellido", None))
+        self.ui.dniline.setText("")
+        self.ui.dniline.setPlaceholderText(QCoreApplication.translate("form", u"Ingrese el DNI", None))
+        self.ui.botonborrar.setText(QCoreApplication.translate("form", u"Borrar", None))
+        self.ui.botonagregar.setText(QCoreApplication.translate("form", u"agregar", None))
+        self.ui.ciudadseleccion.setItemText(0, QCoreApplication.translate("form", u"seleccione ciudad", None))
+        self.ui.ciudadseleccion.setItemText(1, QCoreApplication.translate("form", u"La calera", None))
+        self.ui.ciudadseleccion.setItemText(2, QCoreApplication.translate("form", u"Villa allende", None))
+        self.ui.ciudadseleccion.setItemText(3, QCoreApplication.translate("form", u"Narnia", None))
+
+        self.ui.ciudadseleccion.setCurrentText(QCoreApplication.translate("form", u"seleccione ciudad", None))
+        self.ui.vacunaseleccion.setItemText(0, QCoreApplication.translate("form", u"Seleccione la vacuna", None))
+        self.ui.vacunaseleccion.setItemText(1, QCoreApplication.translate("form", u"SPUTNIK V(1era dosis)", None))
+        self.ui.vacunaseleccion.setItemText(2, QCoreApplication.translate("form", u"SPUTNIK V(2da dosis)", None))
+        self.ui.vacunaseleccion.setItemText(3, QCoreApplication.translate("form", u"ASTRAZENECA(1era dosis)", None))
+        self.ui.vacunaseleccion.setItemText(4, QCoreApplication.translate("form", u"ASTRAZENECA(2da dosis)", None))
+        self.ui.vacunaseleccion.setItemText(5, QCoreApplication.translate("form", u"PFIZER(1era dosis)", None))
+        self.ui.vacunaseleccion.setItemText(6, QCoreApplication.translate("form", u"PFIZER(2da dosis)", None))
+        self.ui.vacunaseleccion.setItemText(7, QCoreApplication.translate("form", u"MODERNA(1era dosis)", None))
+        self.ui.vacunaseleccion.setItemText(8, QCoreApplication.translate("form", u"MODERNA(2da dosis)", None))
+        self.ui.vacunaseleccion.setItemText(9, QCoreApplication.translate("form", u"SINOPHARM(1era dosis) ", None))
+        self.ui.vacunaseleccion.setItemText(10, QCoreApplication.translate("form", u"SINOPHARM(2da dosis) ", None))
+
+    def nombre(self):
+        pass
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = MainWindow()
+    window = Formwindow()
     window.show()
 
     sys.exit(app.exec_())
