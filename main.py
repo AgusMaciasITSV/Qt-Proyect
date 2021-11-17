8e61871c7b2c97f1fa2c59eab4ea733653785260
ProxVix <88912189+ProxVix@users.noreply.github.com>
2021-11-17 08:29:42 -0300

    temp

--- main.py
+++ main.py
@@ -2,21 +2,29 @@ import sys
 from PySide2.QtWidgets import QMainWindow, QApplication
 
 from MMenu.mainMenu import Ui_MainWindow as MenuWindow
-
+from MMenu.exit_dialog import Ui_Dialog as ExDialog
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = MenuWindow()
         self.ui.setupUi(self)
-    def openForm():
+        self.CExit = ConfirmEx()
+
+    def openForm(self):
         pass
     
-    def openLists():
+    def openLists(self):
         pass
 
-    def confirmExit():
-        pass
+    def confirmExit(self):
+        self.CEXit.show()
+
+class ConfirmEx(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = ExDialog()
+        self.ui.setupUi(self)       
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
